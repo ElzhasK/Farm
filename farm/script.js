76ff62a0f80c724c2fcb9b4cc06b2c931bc6757f
Elzhas <103108579+ElzhasK@users.noreply.github.com>
2023-01-10 21:50:10 +0600

    Game for small children

---
+++ farm/script.js
@@ -0,0 +1,39 @@
+function horseSound() {
+  var horse = new Audio("horsesound.mp3");
+  horse.play();
+}
+
+function chickenSound() {
+  var chicken = new Audio("chickensound.mp3");
+  chicken.play();
+}
+
+function cowSound() {
+  var cow = new Audio("cowsound.mp3");
+  cow.play();
+}
+
+function dogSound() {
+  var dog = new Audio("dogsound.mp3");
+  dog.play();
+}
+
+function gooseSound() {
+  var goose = new Audio("goosesound.mp3");
+  goose.play();
+}
+
+function sheepSound() {
+  var sheep = new Audio("sheepsound.mp3");
+  sheep.play();
+}
+
+function pigSound() {
+  var pig = new Audio("pigsound.mp3");
+  pig.play();
+}
+
+function farmerSound() {
+  var farmer = new Audio("farmersound.mp3");
+  farmer.play();
+}
